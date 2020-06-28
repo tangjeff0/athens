e71ea61ac6fa4a3a2ce3a7e0875850dbbc1ca6f0
Jeffery Tang <tangj1122@gmail.com>
2020-06-28 16:36:01 -0400

    refactor: move all datoms into root devcards namespace. transact once

--- src/cljs/athens/devcards/right_sidebar.cljs
+++ src/cljs/athens/devcards/right_sidebar.cljs
@@ -1,16 +1,14 @@
 (ns athens.devcards.right-sidebar
   (:require
     ["@material-ui/icons" :as mui-icons]
-    [athens.db :as db]
+    [athens.devcards.block-page :refer [block-page-component]]
     [athens.devcards.buttons :refer [button-primary]]
     [athens.devcards.node-page :refer [node-page-component]]
     [athens.style :refer [color #_OPACITIES]]
     [cljsjs.react]
     [cljsjs.react.dom]
     [devcards.core :refer [defcard-rg]]
-    [posh.reagent :refer [q]]
     [re-frame.core :refer [dispatch subscribe]]
-    [reagent.core :as r]
     [stylefy.core :as stylefy :refer [use-style #_use-sub-style]]))
 
 
@@ -40,49 +38,38 @@
 
 ;;; Components
 
-(def uids
-  ["OaSVyM_nr"
-   "p1Xv2crs3"])
-
 
 (defn right-sidebar-el
-  [state]
-  (let [uids (->> @(q '[:find ?uid ?title
-                        :in $ [?uid ...]
-                        :where
-                        [?e :block/uid ?uid]
-                        [?e :node/title ?title]]
-                      db/dsdb uids)
-                  vec
-                  (reduce-kv
-                    (fn [m _ [uid title]]
-                      (assoc m uid {:open  false
-                                    :title title}))
-                    {}))
-        s (r/atom {:uids uids})]
-    (when (:open state)
-      [:div (use-style right-sidebar-style)
-       [:div (use-style content-style)
-        (doall
-          (for [[uid {:keys [open title]}] (:uids @s)]
-            ^{:key uid}
-            [:div {:style {:display "flex"}}
-             [:span (use-style toggle-page-style
-                      {:on-click (fn [_]
-                                   (swap! s update-in [:uids uid :open] not))})
-              [:> mui-icons/KeyboardArrowDown]]
-             (if open
+  [open? items]
+  (when open?
+    [:div (use-style right-sidebar-style)
+     [:div (use-style content-style)
+      (doall
+        (for [[uid {:keys [open node/title block/string]}] items
+              :let [node-page? (boolean title)
+                    heading (or title string)]]
+          ^{:key uid}
+          [:div {:style {:display "flex"}}
+           [:span (use-style toggle-page-style
+                    {:on-click #(dispatch [:right-sidebar/toggle-item uid])})
+            [:> mui-icons/KeyboardArrowDown]]
+           (if open
+             (if node-page?
                [node-page-component [:block/uid uid]]
-               [:div [:h1 title]])]))]
-       [:div {:on-click (fn [_] (dispatch [:toggle-right-sidebar]))}
-        [:> mui-icons/Close]]])))
+               [block-page-component [:block/uid uid]])
+             [:div
+              [:h1 heading]])
+           [:span {:on-click #(dispatch [:right-sidebar/close-item uid])}
+            [:> mui-icons/Close]]]))]
+     [:div {:on-click #(dispatch [:right-sidebar/toggle])}
+      [:> mui-icons/Close]]]))
 
 
 (defn right-sidebar-component
   []
-  (let [state @(subscribe [:right-sidebar])]
-    ;;(prn state)
-    [right-sidebar-el state]))
+  (let [open? @(subscribe [:right-sidebar/open])
+        items @(subscribe [:right-sidebar/items])]
+    [right-sidebar-el open? items]))
 
 
 ;;; Devcards
@@ -94,7 +81,7 @@
 
 
 (defcard-rg Toggle
-  [button-primary {:label "Toggle" :on-click-fn #(dispatch [:toggle-right-sidebar])}])
+  [button-primary {:label "Toggle" :on-click-fn #(dispatch [:right-sidebar/toggle])}])
 
 
 (defcard-rg Right-Sidebar
